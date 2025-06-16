9c9857b5dff112ab595978855e162077df9012b6
Phuong Nam <thaidagpnam@gmail.com>
2025-06-16 20:55:43 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,8 +1,12 @@
+
+import "./style.css"
+
 const MyComponent = () => {
   return (
-    <div>
-      iFanIT & em Nam update
-    </div>
+    <>
+        <div style={{padding: 20, backgroundColor: "aqua", borderRadius: 10, boxShadow: "1px 2px 3px 0"}}>iFanIT & em Nam update</div>
+        <div className="child">em name something</div>
+    </>
   );
 }
 

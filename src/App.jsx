a27feda06b0293c6a7cd98f65f67405d633cf325
Phuong Nam <thaidagpnam@gmail.com>
2025-06-16 20:08:16 +0700

    video 29

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
-
-const MyComponent = () => {
-  return (
-    <div>
-      iFanIT & em Nam update
-    </div>
-  );
-}
-
-
+import MyComponent from './components/learn/MyComponent'
+import {SecondComponent, ThirdComponent}  from './components/learn/SecondComponent'
+  
 const App = ()  => {
   const [count, setCount] = useState(0)
   
   return (
-    <>
+    <> 
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,6 +20,8 @@ const App = ()  => {
       </div>
       <h1>Hello world & iFanIT</h1>
       <MyComponent/>
+      <SecondComponent/>
+      <ThirdComponent/>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ src/components/learn/MyComponent.jsx
@@ -0,0 +1,9 @@
+const MyComponent = () => {
+  return (
+    <div>
+      iFanIT & em Nam update
+    </div>
+  );
+}
+
+export default MyComponent;  

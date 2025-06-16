7afb7c0d9dc3c320d1c91c11b7fa9cf7485a5aee
Phuong Nam <thaidagpnam@gmail.com>
2025-06-17 00:07:38 +0700

    video 36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -3,13 +3,26 @@ const TodoNew = (props) => {
   const { addNewTodo } = props;
 
   // addNewTodo("iFanIT");
+  const handleClick = (content) => {
+    alert(`Click me: ${content}`);
+  }
+
+  let content = "";
+  const handleOnChange = (event) => {
+    // alert(`Click me: ${event}`);
+    console.log(">>> on change  ", event)
+  }
+
 
   return (
     <div className="todo-new">
-      <input type="text" placeholder="Enter your task" />
-      <button>Add</button>
+      <input type="text" placeholder="Enter your task" onChange={(e) => handleOnChange(e.target.value)} />
+      <button
+        style={{ cursor: "pointer", }}
+        onClick={handleClick}
+      >Add</button>
     </div>
   );
 }
 
-export default TodoNew;
+export default TodoNew; 

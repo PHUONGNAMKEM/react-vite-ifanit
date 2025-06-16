6f609e5c04c47dc51e06f9321b173c0d73de5576
Phuong Nam <thaidagpnam@gmail.com>
2025-06-16 19:22:04 +0700

    video 28

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+const MyComponent = () => {
+  return (
+    <div>
+      iFanIT & em Nam update
+    </div>
+  );
+}
+
+
 const App = ()  => {
   const [count, setCount] = useState(0)
-
-
-  // function myFunction() {
-  //   console.log(">>> run my function");
-  // }
-
-  const myFunction = () => {
-    console.log(">>> run my arrow function");
-
-  }
-
-  myFunction();
-
+  
   return (
     <>
       <div>
@@ -29,6 +27,7 @@ const App = ()  => {
         </a>
       </div>
       <h1>Hello world & iFanIT</h1>
+      <MyComponent/>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
